289b5902f144758e3ab9e2a9e440540673264a40
Et <1756238821@qq.com>
2019-06-04 13:34:00 +0800

    add new view

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgZorroAntdModule, NZ_I18N, zh_CN } from 'ng-zorro-antd';
 
-
+/** 配置 angular i18n **/
+import { registerLocaleData } from '@angular/common';
+import zh from '@angular/common/locales/zh';
+registerLocaleData(zh);
 
 @NgModule({
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    NgZorroAntdModule,
     RouterModule.forRoot([
       { path: '', pathMatch: 'full', redirectTo: '/enter-leave' },
 
@@ -18,6 +25,10 @@ import { AppComponent } from './app.component';
   declarations: [
     AppComponent,
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  /** 配置 ng-zorro-antd 国际化（文案 及 日期） **/
+  providers   : [
+    { provide: NZ_I18N, useValue: zh_CN }
+  ]
 })
 export class AppModule { }
