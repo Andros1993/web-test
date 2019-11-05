877c9dd7a548d377dfc04c60d3fa5cc7df6e2582
Et <>
2019-11-05 21:58:00 +0800

    update

--- src/app/products/products.component.ts
+++ src/app/products/products.component.ts
@@ -21,27 +21,27 @@ export class ProductsComponent implements OnInit {
 
   ngOnInit(): void {
     // let teststr = "Air_Purifier$19.99.jpg"
-    // let reg = new RegExp('_', "g")
+    // // let reg = new RegExp('_', "g")
     // let name  = teststr.replace('.jpg', '')
     //   .replace(reg, ' ')
     //   .match(/.+$/)
     // console.log(name)
-    // let reg = new RegExp('_', 'g');
-    // let item = 'Air_Purifier$19.99.jpg';
-    //
-    // let name = item.match(/.+(\$)/)[0].substring(0, item.lastIndexOf('$'))
-    //   .replace('.jpg', '')
-    //   .replace('.png', '')
-    //   .replace('.JPG', '')
-    //   .replace('.PNG', '')
-    //   .replace(reg, ' ');
-    //
-    // let price = item.match(/\$.+/)[0]
-    //   .replace('$', '')
-    //   .replace('.jpg', '')
-    //   .replace('.png', '')
-    //   .replace('.JPG', '')
-    //   .replace('.PNG', '')
+    let reg = new RegExp('_', 'g');
+    let item = 'Air_Purifier$19.99.jpg';
+
+    let name = item.match(/.+(\$)/)[0].substring(0, item.lastIndexOf('$'))
+      .replace('.jpg', '')
+      .replace('.png', '')
+      .replace('.JPG', '')
+      .replace('.PNG', '')
+      .replace(reg, ' ');
+
+    let price = item.match(/\$.+/)[0]
+      .replace('$', '')
+      .replace('.jpg', '')
+      .replace('.png', '')
+      .replace('.JPG', '')
+      .replace('.PNG', '')
 
 
     this.requestService.getArticles().subscribe(
